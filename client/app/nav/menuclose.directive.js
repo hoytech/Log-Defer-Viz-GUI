85d68cefa77068ed97a8f46b15df9c1e8e0342ae
Justin Pacheco <justinjpacheco@gmail.com>
2014-12-28 18:23:33 -0500

    added stop-menu-close directive to prevent pref menu from closing when selecting an option

---
+++ client/app/nav/menuclose.directive.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .directive('stopMenuClose', stopMenuCloseDirective);
+
+  function stopMenuCloseDirective() {
+    return {
+      restrict: 'A',
+      link: function ($scope, element, attrs) {
+        element.on('click', function(e) {
+          e.stopPropagation();
+        });
+      }
+    }
+
+  }
+
+})();
